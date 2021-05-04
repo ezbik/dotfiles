362d4d9d02222e160b03c043c79f0674d5e35682
Pavel Piatruk <erik.zbik@gmail.com>
2021-05-04 21:07:35 +0300

    m

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # vim: filetype=bash
 
 
-rsync -av ./ ~/ --exclude .git --exclude README.md
+rsync -av ./ ~/ --exclude .git --exclude README.md --exclude install.sh
 
